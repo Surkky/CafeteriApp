f87d93af4ff47682701edee7a44eb7cf39c31216
Surky <cruz.gcp@gmail.com>
2025-04-08 12:13:47 +0200

    hola

--- CafeteriApp/Program.cs
+++ CafeteriApp/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("CafeteriApp");
+
+            Console.WriteLine("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         }
     }
 }
